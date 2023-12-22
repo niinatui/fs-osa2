dc9737bf030643b5ee3fa7a21646a78b65b88d89
niinatui <niinatuikkanen@gmail.com>
2023-12-22 12:53:39 +0200

    teht 2.16-2.17

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import personService from './services/persons'
+import './index.css'
 
 const Persons = (props) => {
   const persons = props.persons
@@ -39,11 +40,37 @@ const Filter = (props) => {
   )
 }
 
+const ErrorNotification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="notification">
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     personService
@@ -71,6 +98,19 @@ const App = () => {
           setPersons(persons.map(person => person.id !== PersonToChange.id ? person : updated))
           setNewName('')
           setNewNumber('')
+          setMessage(`Changed ${personObject.name} number`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+          })
+        .catch(() => {
+            setErrorMessage(`Information of ${PersonToChange.name}' has already been removed from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setPersons(persons.filter(person => person.name !== PersonToChange.name))
+            setNewName('')
+            setNewNumber('')
           })
       } 
     } else {
@@ -80,6 +120,10 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        setMessage(`Added ${personObject.name}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       })
   }
 }
@@ -99,6 +143,12 @@ const App = () => {
         .deleted(person.id)
         .then(() => {
           personService.getAll().then(response => setPersons(response))
+          setMessage(
+            `Deleted ${person.name}`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         })
     }
   }
@@ -106,7 +156,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
+      <ErrorNotification message={errorMessage} />
       <Filter filter={filter} setFilter={setFilter} />
+      <h2>add a new</h2>
       <PersonForm
         newName={newName}
         newNumber={newNumber}
